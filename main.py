f7a0c80004c8353128dbb42cad4b2224e37e6084
orkooshov <orkooshov@bregis.ru>
2023-02-21 16:52:59 +0600

    edited main

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
-from sqlalchemy.orm import Session
-from app.database.connection import initialize_database, engine
-from app.database.models import User, Task, Subtask
+from app.database.connection import initialize_database
+from app.core.config import FastapiConfig
+from app.api.api import router
 
 
 initialize_database()
-with Session(engine) as dbsession:
-    user = User(username='testa', password='test')
-    dbsession.add(user)
-    task = Task(title='hello', user=user)
-    dbsession.add(task)
-    st = Subtask(title='test_subtask', task=task)
-    dbsession.add(st)
-    dbsession.commit()
+app = FastAPI(**FastapiConfig.get_fastapi_kwargs())
+app.include_router(router)
