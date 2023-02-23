84e2e32efb713c6fb83f45590c6ed75ccde3fdec
orkooshov <orkooshov@bregis.ru>
2023-02-23 22:54:40 +0600

    edit api routers

--- app/api/router/auth.py
+++ app/api/router/auth.py
@@ -1,25 +1,52 @@
 from fastapi.routing import APIRouter
+from fastapi import Depends
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
 from app.schema import auth as s
+from app.utils import dependency as dep
+from app.utils import auth as auth_utils
+from app.database.models import User
+import bcrypt
 
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 @router.post('/get-token')
-async def get_token(body: s.TokenRequest) -> s.TokenResponse:
-    return s.TokenResponse(token=f'{body.username} {body.password}')
+async def get_token(body: s.TokenRequest,
+                    db: Session = Depends(dep.get_db)) -> s.TokenResponse:
+    user = auth_utils.authenticate(db, body.username, body.password)
+    if not user:
+        raise HTTPException(400, 'Incorrect username or password')
+    token = auth_utils.gen_token(user.id)
+    return s.TokenResponse(token=token)
 
 @router.post('/user', status_code=201)
-async def register_user(body: s.RegisterUserRequest) -> s.RegisterUserResponse:
+async def register_user(body: s.RegisterUserRequest,
+                        db: Session = Depends(dep.get_db)) -> s.RegisterUserResponse:
     return s.RegisterUserResponse(token=body.username)
 
-@router.get('/user/{user_id}')
-async def read_user(user_id: int) -> s.ReadUserResponse:
-    return s.ReadUserResponse(id=user_id, username='test')
+@router.get('/user')
+async def read_user(user: User = Depends(dep.authorize)) -> s.ReadUserResponse:
+    return s.ReadUserResponse.from_orm(user)
 
-@router.put('/user/{user_id}', status_code=204)
-async def update_user(user_id: int, body: s.UpdateUserRequest):
-    return
+@router.patch('/user', status_code=204)
+async def update_user(body: s.UpdateUserRequest,
+                      db: Session = Depends(dep.get_db),
+                      user: User = Depends(dep.authorize)):
+    user.username = body.username
+    user.first_name = body.firstName
+    user.last_name = body.lastName
+    user.middle_name = body.middleName
+    user.email = body.email
+    user.gender = body.gender
+    db.commit()
 
-@router.post('/passwd')
-async def update_password(body: s.UpdatePasswordRequest) -> s.UpdatePasswordResponse:
-    return s.UpdatePasswordResponse(token=body.new_password)
+@router.post('/passwd', status_code=204)
+async def update_password(body: s.UpdatePasswordRequest,
+                          db: Session = Depends(dep.get_db),
+                          user: User = Depends(dep.authorize)):
+    user = auth_utils.authenticate(db, user.username, body.oldPassword)
+    if not user:
+        raise HTTPException(400, 'Incorrect password')
+    user.password = bcrypt.hashpw(body.newPassword.encode(), bcrypt.gensalt())
+    db.commit()
