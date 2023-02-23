b02d34672074fba781ab575e550a9506aac0def0
orkooshov <orkooshov@bregis.ru>
2023-02-24 03:30:26 +0600

    add some endpoints

--- app/api/router/task.py
+++ app/api/router/task.py
@@ -1,11 +1,32 @@
+from fastapi import Depends
 from fastapi.routing import APIRouter
-from app.database.models import Task
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+from app.database import models as m
+from app.utils import task as task_utils, dependency as dep
+from app.schema import task as s
 
 
 router = APIRouter(prefix='/task', tags=['task'])
 
-# @router.get('/')
-# async def get_tasks():
-#     with create_session() as dbs:
-#         tasks = dbs.query(Task).all()
-#     return tasks
+@router.get('/')
+async def list_tasks(db: Session = Depends(dep.get_db), 
+                    user: m.User = Depends(dep.authorize)) -> list[s.Task]:
+    tasks = task_utils.get_user_tasks(db, user.id)
+    resp = (s.Task.from_orm(i) for i in tasks)
+    return resp
+
+@router.get('/{task_id}')
+async def retrieve_task(*, db: Session = Depends(dep.get_db),
+                        user: m.User = Depends(dep.authorize),
+                        task_id: int) -> s.Task:
+    task = task_utils.get_task(db, user.id, task_id)
+    if not task:
+        raise HTTPException(404, 'task not found')
+    return s.Task.from_orm(task)
+
+# @router.post('/', status_code=201)
+# async def create_task(*, db: Session = Depends(dep.get_db),
+#                       user: m.User = Depends(dep.authorize),
+#                       body: s.Task) -> s.Task:
+#     pass
