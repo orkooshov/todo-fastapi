0a5c3e064cf9e1bc5d80031f036e670a55024e30
orkooshov <orkooshov@bregis.ru>
2023-02-24 00:26:04 +0600

    refactor

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,4 +1,4 @@
-from bcrypt import checkpw
+from bcrypt import checkpw, hashpw, gensalt
 from sqlalchemy.orm import Session
 from datetime import datetime as dt, timedelta
 import jwt
@@ -13,13 +13,47 @@ def get_user(db: Session, username: str) -> m.User | None:
 def get_user_by_id(db: Session, id: int) -> m.User | None:
     return db.query(m.User).filter(m.User.id == id).first()
 
+def register_user(db: Session, username: str, password: str) -> m.User:
+    user = m.User(username=username, 
+                  password=hashpw(password.encode(), gensalt()))
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
 def authenticate(db: Session, username: str, password: str) -> m.User | None:
-    try:
-        user = get_user(db, username)
-        if user and checkpw(password.encode(), user.password.encode()):
-            return user
-    except:
-        pass
+    user = get_user(db, username)
+    if user and checkpw(password.encode(), user.password):
+        return user
+
+def update_user(db: Session, user: m.User, 
+                username: str = None, first_name: str = None, 
+                last_name: str = None, middle_name: str = None,
+                email: str = None, gender: m.Gender = None) -> m.User:
+    user.username = username or user.username
+    user.first_name = first_name or user.first_name
+    user.last_name = last_name or user.last_name
+    user.middle_name = middle_name or user.middle_name
+    user.email = email or user.email
+    user.gender = gender or user.gender
+    db.commit()
+    db.refresh(user)
+    return user
+
+def update_password(db: Session, user: m.User, 
+                    old_password: str, new_password: str) -> m.User | None:
+    user = authenticate(db, user.username, old_password)
+    if not user:
+        return None
+    user.password = hashpw(new_password.encode(), gensalt())
+    db.commit()
+    db.refresh(user)
+    return user
+
+def get_token(db: Session, username: str, password: str) -> str | None:
+    user = authenticate(db, username, password)
+    if user:
+        return gen_token(user.id)
 
 def gen_token(userId: int) -> str:
     now = dt.utcnow()
