e8a4af56cd42f627db69e24e51346137d51e0181
orkooshov <orkooshov@bregis.ru>
2023-02-24 03:30:13 +0600

    update schema.task

--- app/schema/task.py
+++ app/schema/task.py
@@ -9,8 +9,12 @@ class BaseTask(BaseModel):
     is_done: bool = Field(False, alias='isDone')
     is_favorite: bool = Field(False, alias='isFavorite')
     date_until: dt | None = Field(None, alias='dateUntil')
-    created_at: dt | None = Field(None, 'createdAt')
-    updated_at: dt | None = Field(None, 'updatedAt')
+    created_at: dt | None = Field(None, alias='createdAt')
+    updated_at: dt | None = Field(None, alias='updatedAt')
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
 
 
 class Task(BaseTask):
