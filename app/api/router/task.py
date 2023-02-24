31a60d680196e23672a898b33ca2517a657b3a1d
orkooshov <orkooshov@bregis.ru>
2023-02-24 20:06:29 +0600

    add task endpoints

--- app/api/router/task.py
+++ app/api/router/task.py
@@ -25,8 +25,29 @@ async def retrieve_task(*, db: Session = Depends(dep.get_db),
         raise HTTPException(404, 'task not found')
     return s.Task.from_orm(task)
 
-# @router.post('/', status_code=201)
-# async def create_task(*, db: Session = Depends(dep.get_db),
-#                       user: m.User = Depends(dep.authorize),
-#                       body: s.Task) -> s.Task:
-#     pass
+@router.post('/', status_code=201)
+async def create_task(*, db: Session = Depends(dep.get_db),
+                      user: m.User = Depends(dep.authorize),
+                      body: s.Task) -> s.Task:
+    kwargs = body.dict()
+    kwargs.pop('id')
+    task = task_utils.create_task(db, user.id, **kwargs)
+    return s.Task.from_orm(task)
+
+@router.patch('/{task_id}')
+async def update_task(*, db: Session = Depends(dep.get_db),
+                      user: m.User = Depends(dep.authorize),
+                      task_id: int,
+                      body: s.Task) -> s.Task:
+    kwargs = body.dict()
+    kwargs.pop('id')
+    task = task_utils.update_task(db, user.id, task_id, **kwargs)
+    if not task:
+        raise HTTPException(404, 'task not found')
+    return s.Task.from_orm(task)
+
+@router.delete('/{task_id})')
+async def delete_task(*, db: Session = Depends(dep.get_db),
+                      user: m.User = Depends(dep.authorize),
+                      task_id: int) -> None:
+    task_utils.delete_task(db, user.id, task_id)
--- app/utils/task.py
+++ app/utils/task.py
@@ -1,14 +1,37 @@
 from sqlalchemy.orm import Session
-from app.database.models import Task
+from app.database import models as m
 
 
-def get_user_tasks(db: Session, user_id: int) -> list[Task]:
-    return db.query(Task).filter(Task.user_id == user_id).all()
+def get_user_tasks(db: Session, user_id: int) -> list[m.Task]:
+    return db.query(m.Task).filter(m.Task.user_id == user_id).all()
 
-def get_task(db: Session, user_id: int, task_id: int) -> Task | None:
+def get_task(db: Session, user_id: int, task_id: int) -> m.Task | None:
     return (db
-        .query(Task)
-        .filter(Task.id == task_id and Task.user_id == user_id)
+        .query(m.Task)
+        .filter(m.Task.id == task_id and m.Task.user_id == user_id)
         .first())
 
-# def create_task(db: Session, )
+def create_task(db: Session, user_id: int, **kwargs) -> m.Task:
+    task = m.Task(user_id=user_id, **kwargs)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
+
+def update_task(db: Session, user_id: int, task_id: int, **kwargs) -> m.Task | None:
+    task = get_task(db, user_id, task_id)
+    if not task:
+        return None
+    task.title = kwargs.get('title', task.title)
+    task.description = kwargs.get('description', task.description)
+    task.is_done = kwargs.get('is_done', task.is_done)
+    task.is_favorite = kwargs.get('is_favorite', task.is_favorite)
+    task.date_until = kwargs.get('date_until', task.date_until)
+    db.commit()
+    db.refresh(task)
+    return task
+
+def delete_task(db: Session, user_id: int, task_id: int) -> None:
+    task = get_task(db, user_id, task_id)
+    if task:
+        db.delete(task)
