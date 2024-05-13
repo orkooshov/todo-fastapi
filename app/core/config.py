28d2f3332d87e902ed3a924cf78fc4a7a8f48e50
orkooshov <orkooshov@bregis.ru>
2024-05-14 00:01:17 +0600

    refactor: make minor changes

--- app/core/config.py
+++ app/core/config.py
@@ -9,11 +9,11 @@ load_dotenv()
 class DatabaseConfig:
     dialect = 'postgresql'
     driver = 'psycopg2'
-    user = environ['db_user']
-    password = environ['db_pass']
-    host = environ['db_host']
-    port = environ.get('db_port', '5432')
-    database_name = environ['db_name']
+    user = environ['DB_USER']
+    password = environ['DB_PASS']
+    host = environ['DB_HOST']
+    port = environ.get('DB_PORT', '5432')
+    database_name = environ['DB_NAME']
     echo = False
     pool_size = 10
 
@@ -48,5 +48,5 @@ class FastapiConfig:
 
 
 class AppConfig:
-    api_secret_key = environ['api_secret_key']
+    api_secret_key = environ['API_SECRET_KEY']
     api_key_expire_time = timedelta(days=30)
