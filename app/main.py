1cd7ea3714e9ee2d25321d827d4d4c0bd81ed895
orkooshov <orkooshov@bregis.ru>
2024-05-15 12:52:30 +0600

    refactor(backend): move docker files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
 from app.database.connection import initialize_database
 from app.core.config import FastapiConfig
 from app.api.api import router
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "*"
+]
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+http_bearer = HTTPBearer()
 
 initialize_database()
 app = FastAPI(**FastapiConfig.get_fastapi_kwargs())
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
